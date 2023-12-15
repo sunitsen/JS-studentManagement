5e6101473be0ed8254c7597c8a172610bcfc1302
sunit sen <sunitsen50@gmail.com>
2023-12-15 21:56:06 +0600

    message

--- javascript/main.js
+++ javascript/main.js
@@ -1,71 +1,46 @@
-// Initialize variables to keep track of input, operation, and result display status
-let currentInput = '';
-let currentOperation = '';
-let resultDisplayed = false;
+/* Date */
+var date = new Date();
+var currentDay = date.getDate();
+var currentMonth = date.getMonth() + 1; // Adding 1 to adjust for zero-based indexing
+var currentYear = date.getFullYear();
+var currentHours = date.getHours();
+var currentMinutes = date.getMinutes();
+var currentSeconds = date.getSeconds();
 
-// Function to append a value to the display
-function appendToDisplay(value) {
-    // If a result is displayed, clear the display before appending new input
-    if (resultDisplayed) {
-        clearDisplay();
-        resultDisplayed = false;
-    }
-    // Append the provided value to the current input
-    currentInput += value;
-    // Update the display with the current input
-    document.getElementById('display').value = currentInput;
+// Create a span element for the full date
+const fullDate = document.createElement("span");
+fullDate.textContent = " Date:" + " " + currentDay + "/" + currentMonth + "/" + currentYear;
+
+// Select the h1 element inside the element with id "displayDate"
+const displayDateElement = document.querySelector("#displayDate h1");
+
+// Append the fullDate span to the displayDate element
+displayDateElement.appendChild(fullDate);
+
+// Function to format time
+function formatTime(hours, minutes, seconds) {
+  return `${'Time: '}${hours}:${minutes}:${seconds}`;
 }
 
-// Function to set the operation (e.g., +, -, *, /)
-function setOperation(operation) {
-    // Check if there is existing input before setting the operation
-    if (currentInput !== '') {
-        // Update the current operation and append it to the input for display
-        currentOperation = operation;
-        currentInput += operation;
-        document.getElementById('display').value = currentInput;
-    }
+// Function to update time in the display
+function updateTime() {
+  var currentTime = new Date();
+  var currentHours = currentTime.getHours();
+  var currentMinutes = currentTime.getMinutes();
+  var currentSeconds = currentTime.getSeconds();
+
+  // Create a span element for the time
+  const displayTimeElement = document.querySelector("#displayTime h1");
+  const fullTime = document.createElement("span");
+  fullTime.textContent = formatTime(currentHours, currentMinutes, currentSeconds);
+
+  // Replace the content of the displayTime element
+  displayTimeElement.innerHTML = '';
+  displayTimeElement.appendChild(fullTime);
 }
 
-// Function to add a decimal point to the current input
-function setDecimal() {
-    // If a result is displayed, clear the display before adding a decimal
-    if (resultDisplayed) {
-        clearDisplay();
-        resultDisplayed = false;
-    }
-    // Check if the current input already includes a decimal point
-    if (!currentInput.includes('.')) {
-        // If not, add a decimal point to the current input
-        currentInput += '.';
-        // Update the display with the current input
-        document.getElementById('display').value = currentInput;
-    }
-}
+// Initial update
+updateTime();
 
-// Function to clear the display and reset variables
-function clearDisplay() {
-    // Reset input, operation, and result display status
-    currentInput = '';
-    currentOperation = '';
-    resultDisplayed = false;
-    // Clear the display on the HTML page
-    document.getElementById('display').value = '';
-}
-
-// Function to perform the calculation when the '=' button is clicked
-function calculate() {
-    try {
-        // Use the eval function to evaluate the current input as a mathematical expression
-        const result = eval(currentInput);
-        // Update the display with the calculated result
-        document.getElementById('display').value = result;
-        // Update the current input with the result for potential further calculations
-        currentInput = result.toString();
-        // Set the result display status to true
-        resultDisplayed = true;
-    } catch (error) {
-        // If an error occurs during evaluation, display 'Error' on the calculator
-        document.getElementById('display').value = 'Error';
-    }
-}
+// Set interval to update time every second
+setInterval(updateTime, 1000);
